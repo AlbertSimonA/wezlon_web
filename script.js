14384a29abed6d86944a6cff525262f59d96b4b1
robin-UI <robinbiju1901@gmail.com>
2024-10-12 08:22:55 +0530

    all pages

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const mobileNav = document.getElementById("mobile-nav");
+
 setTimeout(function () {
   document.getElementById("form-container").style.display = "block";
 }, 10000);
+
+
+function menuBtnFunction(menuBtn) {
+  menuBtn.classList.toggle("active");
+  mobileNav.hidden = !mobileNav.hidden 
+}
+
